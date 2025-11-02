95dbe617b37061f720bf8b34c2c2de9cb5500de8
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-11-02 22:48:20 +0000

    feat: login and register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './shared/pages/login-page/login-page';
+import { RegisterPage } from './shared/pages/register-page/register-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginPage
+    },
+    {
+        path: "register",
+        component: RegisterPage
+    }
+];
---
+++ src/app/shared/pages/register-page/register-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-register-page',
+  imports: [RouterLink],
+  templateUrl: './register-page.html',
+  styleUrl: './register-page.css',
+})
+export class RegisterPage {
+
+}
