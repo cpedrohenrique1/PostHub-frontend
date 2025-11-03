c2613c6e629fc0d488e3771202d1d0d3a6663ad3
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-11-03 00:02:55 +0000

    feat: feed page and main header components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './shared/pages/login-page/login-page';
 import { RegisterPage } from './shared/pages/register-page/register-page';
+import { FeedPage } from './shared/pages/feed-page/feed-page';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: FeedPage
+    },
     {
         path: "login",
         component: LoginPage
---
+++ src/app/shared/pages/feed-page/feed-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainHeader } from "../../components/main-header/main-header";
+
+@Component({
+  selector: 'app-feed-page',
+  imports: [MainHeader],
+  templateUrl: './feed-page.html',
+  styleUrl: './feed-page.css',
+})
+export class FeedPage {
+
+}
